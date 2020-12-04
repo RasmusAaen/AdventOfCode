7f35c6e41aa81869f1c63e261548861a2bad4c73
Rasmus Aaen <rasmus.aaen@proactive.dk>
2020-12-05 00:35:04 +0100

    Day4

---
+++ 2015/04/part2.ps1
@@ -0,0 +1,16 @@
+$in = "bgvyzdsv"
+
+$md5 = [System.Security.Cryptography.HashAlgorithm]::Create("MD5")
+$enc = [system.Text.Encoding]::UTF8
+
+$i=0
+while($true) {
+    if($i%100000 -eq 0) {Write-Host -NoNewline '.'}
+    $data = $enc.GetBytes("$($in)$($i)") 
+    $hash = [System.Convert]::ToHexString($md5.ComputeHash($data))
+    if($hash -match "^0{6}") {
+        Write-Host ("Lowest number to give a hash starting with 6 zeros: {0}" -f $i)
+        break
+    }
+    $i++
+}
