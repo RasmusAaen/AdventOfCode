e0acd556727fb44f10d0e88e672a118af62a670e
Rasmus Aaen <rasmus.aaen@proactive.dk>
2020-12-07 17:16:52 +0100

    Vastly faster version

--- 2020/07/part1.ps1
+++ 2020/07/part1.ps1
@@ -2,48 +2,44 @@ $in = Get-Content .\input.txt
 
 #parse rules
 $global:rules = @()
-foreach($line in $in) {
-    $color,$rest = ($line -split ' bags contain ')
+foreach ($line in $in) {
+    $color, $rest = ($line -split ' bags contain ')
     $b = [PSCustomObject]@{
-        Color = [string]$color.Trim()
-        Content = @{}
+        Color      = [string]$color.Trim()
+        Content    = @{}
+        CanContain = @()
     }
-    foreach($c in ($rest -split ,',')) {
-        if($c -match "(?<num>\d+) (?<color>.+) bag") {
-            $b.Content.Add([string]$Matches.color.Trim(),[int]$Matches.num)
+    foreach ($c in ($rest -split , ',')) {
+        if ($c -match "(?<num>\d+) (?<color>.+) bag") {
+            $b.Content.Add([string]$Matches.color.Trim(), [int]$Matches.num)
         }
     }
     $global:rules += $b
 }
 
-#Very iniefficient way - I should store the recursive results for each bag as we go
-#instead of calculating every one again and again.
-function canContain([string]$find,[string]$bag) {
-    $b = $Global:rules | Where-Object -Property Color -EQ -Value $bag
-    if($b.Content.ContainsKey($find)) {
-        return $true
-    }
-    $found = $false
-    foreach($c in $b.Content.Keys) {
-        if(canContain $find $c) {
-            $found = $true
-            break
+#Add canContain info to each bag type 
+function getCanContain([object]$bag) {
+    $idx = $global:rules.IndexOf($bag)
+    foreach ($color in $bag.Content.Keys) {
+        $global:rules[$idx].canContain += $color
+        $b = $global:rules | where { $_.color -eq $color }
+        if ($b.canContain.length -eq 0) {
+            getCanContain $b
         }
+        $global:rules[$idx].canContain += $b.CanContain
     }
-    return $found
+    $global:rules[$idx].canContain = $global:rules[$idx].canContain | select -Unique
+}
+
+$c=0
+foreach ($bag in $Global:rules) {
+    Write-Progress -Activity "Processing bag colors" -Status $bag.Color -PercentComplete (($c++/$global:rules.Count)*100)
+    if ($bag.CanContain.Length -eq 0) {
+        getCanContain $bag
+    }
 }
 
 $find = "shiny gold"
-$count  = 0
-$progress = 0
-foreach($bag in $Global:rules) {
-    #write-host "." -NoNewLine
-    Write-Progress -Activity "Finding bags..." -Status "Researching $($bag.Color)" -PercentComplete (($progress++/$global:rules.Count)*100)
-    if(canContain $find $bag.Color) {
-        $count++
-    }
-}
+$count = ($global:rules | where{$_.canContain -contains $find}).count
 
-Write-Output ("Number of bags that can contain a {0} bag is {1}" -f $find,$count)
-
-
+Write-Host ("Found {0} bags that can contain a {1} bag" -f $count,$find)
