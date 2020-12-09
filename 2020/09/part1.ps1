38c099b3d5b6bd29362445b3a7f43ed191c5e5f4
Rasmus Aaen <rasmus.aaen@proactive.dk>
2020-12-09 17:46:14 +0100

    Day 9

---
+++ 2020/09/part1.ps1
@@ -0,0 +1,37 @@
+$in = [Int64[]](Get-Content .\input.txt)
+
+$preamble = 25
+$last = 25
+
+function isValid ([Int64]$num,[Int64[]]$list) {
+    for($i=0;$i -lt $list.Length; $i++) {
+        for($j=0;$j -lt $list.Length; $j++) {
+            if(($i -ne $j) -and ($list[$i] + $list[$j] -eq $num)) {
+                return $true
+            }
+        }
+    }
+    return $false
+}
+
+for($i=$preamble;$i -lt $in.Length; $i++) {
+    $valid = isValid $in[$i] $in[($i-$last)..($i-1)]
+    if(!$valid) {
+        write-host ("First invalid number is {0}" -f $in[$i])
+        for($x=0;$x -lt $in.Length; $x++) {
+            [int64]$res = 0
+            $y=$x
+            while($res -lt $in[$i]) {
+                $res += $in[$y++]
+            }
+            if($res -eq $in[$i]) {
+                $y--
+                [int64]$small = $in[$x..$y] | sort | select -First 1
+                [int64]$large = $in[$x..$y] | sort | select -Last 1
+                write-host ("smallest and largest number in range is {0} and {1}. Sum is {2}" -f $small,$large,([int64]$small+[int64]$large))
+                break
+            }
+        }
+        break
+    }
+}
