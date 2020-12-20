c2310e4d9aaecaf70231d89f6559dd46a1757975
Rasmus Aaen <rasmus@aaen.net>
2020-12-20 01:52:26 +0100

    Day 19 part 2

--- 2020/19/part2.ps1
+++ 2020/19/part2.ps1
@@ -1,11 +1,11 @@
-$in = Get-Content .\test2.txt
+$in = Get-Content .\input2.txt
 
 $global:rules = @{}
 $messages = @()
 $i = 0
 while ($in[$i] -ne '') {
-    $r1,$r2 = $in[$i++].split(': ')
-    $global:rules.Add([string]$r1,[string]$r2)
+    $r1, $r2 = $in[$i++].split(': ')
+    $global:rules.Add([string]$r1, [string]$r2)
 }
 $i++
 while ($i -lt $in.Length) {
@@ -13,16 +13,15 @@ while ($i -lt $in.Length) {
 }
 
 $global:mem = @{}
-$global:recur8 = 0
-$global:recur11 = 0
+$global:maxDepth = 60
 
-function parseRule([string]$num) {
-    if($global:recur8 -gt 10 -or $global:recur11 -gt 10) {
-        return $null
-    }
+function parseRule([string]$num, [int]$depth = 0) {
     if ($global:mem.containsKey($num)) {
         #We have seen this before - return value from memory
         return $global:mem[$num]
+    }
+    elseif ($depth -gt $global:maxDepth) {
+        return $null
     }
     elseif ($num -match '^\d+$') {
         #single rule number
@@ -32,43 +31,62 @@ function parseRule([string]$num) {
             $val = $Matches.val
         }
         else {
-            $val = parseRule $r
+            $val = parseRule $r ($depth + 1)
         }
-        $global:mem.Add($num, $val)
+        if ($null -ne $val) {
+            if (!$global:mem.ContainsKey($num)) {
+                $global:mem.Add($num, $val)
+            }
+        }
         return $val
     }
     elseif ($num.indexOf('|') -ge 0) {
         #List with options
-        $val = "("
-        $val += ($num.split(' | ') | % { parseRule $_ }) -join '|'
-        $val += ")"
-        $global:mem.Add($num, $val)
-        return $val
-    }
-    else {
-        #list of numbers
-        $ret = ""
-        foreach ($p in $num.split(' ').Trim()) {
-            if($p -eq "8") {
-                $global:recur8++
-            } elseif ($p -eq "11") {
-                $global:recur11++
+        $ret = @()
+        $parts = $num.split(' | ')
+        foreach ($part in $parts) {
+            $r = parseRule $part ($depth + 1)
+            if ($null -ne $r) {
+                $ret += $r
             }
-            $ret += parseRule $p
         }
-        $global:mem.Add($num, $ret)
-        return $ret
+        $val = $null
+        if ($ret.length -gt 0) {
+            $val = "("
+            $val += $ret -join '|'
+            $val += ")"
+            if (!$global:mem.ContainsKey($num)) {
+                    $global:mem.Add($num, $val)
+                }
+            }
+            return $val
+        }
+        else {
+            #list of numbers
+            $ret = ""
+            foreach ($p in $num.split(' ').Trim()) {
+                $ret += parseRule $p ($depth + 1)
+            }
+            if ($ret.Length -gt 0) {
+                if (!$global:mem.ContainsKey($num)) {
+                    $global:mem.Add($num, $ret)
+                }
+            }
+            else {
+                $ret = $null
+            }
+            return $ret
+        }
     }
-}
 
-$pattern = "^" + (parseRule "0") + "$"
-write-host $pattern
+    $pattern = "^" + (parseRule "0") + "$"
+    #write-host $pattern
 
-$validMessages = 0
-foreach($m in $messages) {
-    if($m -match $pattern) {
-        $validMessages++
+    $validMessages = 0
+    foreach ($m in $messages) {
+        if ($m -match $pattern) {
+            $validMessages++
+        }
     }
-}
 
-write-host ("Found {0} valid messages." -f $validMessages)
+    write-host ("Found {0} valid messages." -f $validMessages)
