dbf7cad5d279c1d682fcf5c4f97d19fe0031655b
Rasmus Aaen <rasmus@aaen.net>
2021-12-02 23:19:09 +0100

    2020 python

---
+++ 2020/python/day8.py
@@ -0,0 +1,82 @@
+﻿import re
+from aoc import input_as_lines
+
+def part1(values: list) -> int:
+    ins = 0
+    acc = 0
+    run = []
+    while True:
+        if ins in run:
+            break
+        run.append(ins)
+        (op, v) = re.match(r'^(\w+) ([+-]\d+)$',values[ins]).groups()
+        if op == 'nop':
+            ins += 1
+            continue
+        if op == 'acc':
+            acc += int(v)
+            ins += 1
+            continue
+        if op == 'jmp':
+            ins += int(v)
+            continue
+    return acc
+
+def part2(values: list[str]) -> int:
+    changeInstr = 0
+    while changeInstr < len(values):
+        # Replace operation
+        aList = values.copy()
+        found = False
+        (op, v) = re.match(r'^(\w+) ([+-]\d+)$',aList[changeInstr]).groups()
+        if op == 'nop':
+            aList[changeInstr] = f"jmp {v}"
+            found = True
+        elif op == 'jmp':
+            aList[changeInstr] = f"nop {v}"
+            found = True
+        if found:
+            #Try running the program to see if it works
+            ins = 0
+            acc = 0
+            run = []
+            while True:
+                if ins >= len(aList):
+                    return acc
+                if ins in run:
+                    break
+                run.append(ins)
+                (op, v) = re.match(r'^(\w+) ([+-]\d+)$',aList[ins]).groups()
+                if op == 'nop':
+                    ins += 1
+                    continue
+                if op == 'acc':
+                    acc += int(v)
+                    ins += 1
+                    continue
+                if op == 'jmp':
+                    ins += int(v)
+                    continue
+
+        changeInstr += 1
+
+
+def test():
+    test_input = """nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6""".split('\n')
+    assert part1(test_input) == 5
+    assert part2(test_input) == 8
+
+
+if __name__ == "__main__":
+    test()
+    vals = input_as_lines('C:\\Repos\\Privat\\AdventOfCode\\2020\\python\\input_day8.txt')
+    print(f"Part 1: {part1(vals)}")
+    print(f"Part 2: {part2(vals)}")
---
+++ 2020/python/day9.py
@@ -0,0 +1,40 @@
+﻿import re
+from aoc import input_as_ints
+
+def part1(values: list, preamble: int) -> int:
+    for i in range(preamble, len(values)):
+        found = False
+        for j in range(i-preamble, i):
+            if found: break
+            for k in range(j+1, i):
+                if values[j] + values[k] == values[i]:
+                    found = True
+                    break
+        if not found: break
+
+    return values[i]
+
+def part2(values: list[str], target: int) -> int:
+    for i in range(len(values)):
+        res = [values[i]]
+        for j in range(i+1, len(values)):
+            res.append(values[j])
+            if sum(res) >= target:
+                break
+        if sum(res) == target:
+            break
+    res.sort()
+    return res[0] + res[-1]
+
+
+def test():
+    test_input = [35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576]
+    assert part1(test_input, 5) == 127
+    assert part2(test_input, 127) == 62
+
+
+if __name__ == "__main__":
+    test()
+    vals = input_as_ints('C:\\Repos\\Privat\\AdventOfCode\\2020\\python\\input_day9.txt')
+    print(f"Part 1: {part1(vals, 25)}")
+    print(f"Part 2: {part2(vals, 21806024)}")
