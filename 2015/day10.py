263fa67178957ccc719e96ffc2f13f139e322190
Rasmus Aaen <rasmus@aaen.net>
2021-12-05 16:07:15 +0100

    2015 day 10 and 11

---
+++ 2015/day10.py
@@ -0,0 +1,34 @@
+﻿from aoc import *
+
+def part1(values: str, it: int) -> int:
+    res = values
+    for _ in range(it):
+        pos = 0
+        res = ''
+        while pos < len(values):
+            cnt = 1
+            for j in range(pos+1,len(values)):
+                if values[j] == values[pos]:
+                    cnt += 1
+                else:
+                    break
+
+            res += str(cnt) + values[pos]
+            pos += cnt
+        values = res
+
+    return len(res)
+
+def part2(values: list) -> int:
+    return 1
+
+def test():
+    test_input = '111221'
+    assert part1(test_input, 1) == '312211'
+    #assert part2(test_input) == 3
+
+if __name__ == "__main__":
+    #test()
+    #vals = input_as_lines('C:\\Repos\\Privat\\AdventOfCode\\2015\\input_day10.txt')
+    print(f"Part 1: {part1('1113122113', 40)}")
+    print(f"Part 2: {part1('1113122113', 50)}")
---
+++ 2015/day11.py
@@ -0,0 +1,65 @@
+﻿import re
+from aoc import *
+
+def hasStraight(val: str) -> bool:
+    i = 0
+    while i < len(val)-2:
+        if val[i+1] == chr(ord(val[i])+1):
+            if val[i+2] == chr(ord(val[i+1])+1):
+                return True
+        i += 1
+    return False
+
+def hasInvalid(val: str) -> bool:
+    if re.findall(r'[iol]+',val):
+        return True
+    return False
+
+def hasRepeat(val: str) -> bool:
+    cnt = i = 0
+    c = ''
+    while i < len(val)-1 and cnt < 2:
+        if val[i] == val[i+1] and val[i] != c:
+            cnt += 1
+            i += 1
+            c = [val[i]]
+        i += 1
+    return cnt == 2
+
+def isValid(val: str) -> bool:
+    if hasStraight(val) and not hasInvalid(val) and hasRepeat(val):
+        return True
+    return False
+
+def increase(val: str) -> str:
+    pos = -1
+    p = list(val)
+    while pos >= len(p)*-1:
+        if p[pos] != 'z':
+            p[pos] = chr(ord(p[pos])+1)
+            break
+        else:
+            p[pos] = 'a'
+            pos -= 1
+    return "".join(p)
+
+def part1(val: str) -> int:
+    p = val
+    while True:
+        p = increase(p)
+        if isValid(p):
+            return p
+
+def part2(values: list) -> int:
+    return 1
+
+def test():
+    test_input = 'abcdefgh'
+    assert part1(test_input) == 'abcdffaa'
+    #assert part2(test_input) == 3
+
+if __name__ == "__main__":
+    test()
+    #vals = input_as_lines('C:\\Repos\\Privat\\AdventOfCode\\2015\\input_day11.txt')
+    print(f"Part 1: {part1('hepxcrrq')}")
+    print(f"Part 2: {part1('hepxxyzz')}")
