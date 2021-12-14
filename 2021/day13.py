224f5dec388a13b890b5390967879af65292331c
Rasmus Aaen <rasmus@aaen.net>
2021-12-14 13:11:36 +0100

    2021 day 13+14

---
+++ 2021/day13.py
@@ -0,0 +1,98 @@
+﻿import re
+import numpy
+from aoc import *
+
+def parseInput(values: list[str]):
+    points = []
+    folds = []
+
+    i = 0
+    while i < len(values):
+        if values[i] != '':
+            if values[i].startswith('fold'):
+                a,b = re.match(r'^fold along ([xy])=(\d+)',values[i]).groups()
+                folds.append((a,int(b)))
+            else:
+                x,y = map(int, values[i].split(','))
+                points.append((x,y))
+        i += 1
+
+    maxX = max([x[0] for x in points])
+    maxY = max([x[1] for x in points])
+
+    vals = numpy.zeros((maxY+1,maxX+1), dtype=int)
+    for p in points:
+        vals[p[1]][p[0]] = 1
+
+    return vals, folds
+
+def fold(grid, ax):
+    axis = 1
+    if ax[0] == 'y':
+        axis = 0
+
+    g1, _, g2, _ = numpy.split(grid, [ax[1],ax[1]+1,grid.shape[axis]], axis)
+    g2 = numpy.flip(g2, axis)
+
+    if g1.shape[axis] < g2.shape[axis]:
+        if axis == 0:
+            g1 = numpy.pad(g1,((g2.shape[axis]-g1.shape[axis],0),(0,0)))
+        else:
+            g1 = numpy.pad(g1,((0,0),(g2.shape[axis]-g1.shape[axis],0)))
+    elif g2.shape[axis] < g1.shape[axis]:
+        if axis == 0:
+            g2 = numpy.pad(g2,((g1.shape[axis]-g2.shape[axis],0),(0,0)))
+        else:
+            g2 = numpy.pad(g2,((0,0),(g1.shape[axis]-g2.shape[axis],0)))
+
+    return numpy.logical_or(g1,g2).astype(int)
+
+
+def part1(values: list) -> int:
+    vals, folds = parseInput(values)
+
+    v1 = fold(vals, folds[0])
+    #v1 = fold(v1, folds[1])
+
+    return numpy.count_nonzero(v1)
+
+
+def part2(values: list) -> int:
+    vals, folds = parseInput(values)
+    for f in folds:
+        vals = fold(vals, f)
+    [print(''.join(map(str,x))) for x in vals]
+    return None
+
+
+def test():
+    test_input = """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5""".split('\n')
+    assert part1(test_input) == 17
+    assert part2(test_input) == None
+
+
+if __name__ == "__main__":
+    test()
+    vals = input_as_lines('C:\\Repos\\Privat\\AdventOfCode\\2021\\input_day13.txt')
+    print(f"Part 1: {part1(vals)}")
+    print(f"Part 2: {part2(vals)}")
---
+++ 2021/day14.py
@@ -0,0 +1,79 @@
+﻿import collections
+from typing import Counter, DefaultDict
+from aoc import *
+
+def parseInput(values: list):
+    inserts = {}
+    seq = values[0].strip()
+
+    for i in range(2,len(values)):
+        a,b = values[i].split(' -> ')
+        inserts[a] = b
+
+    return inserts, seq
+
+def part1(values: list, rounds: int = 10) -> int:
+    ins, seq = parseInput(values)
+
+    for _ in range(rounds):
+        newseq = ''
+        for i in range(len(seq)-1):
+            newseq += seq[i]
+            newseq += ins[seq[i:i+2]]
+        newseq += seq[-1]
+        seq = newseq
+
+    counts = collections.defaultdict(int)
+    for ch in seq:
+        counts[ch] += 1
+
+    return max(counts.values()) - min(counts.values())
+
+
+
+def part2(values: list) -> int:
+    ins, seq = parseInput(values)
+
+    charcounts = collections.Counter(seq)
+    paircounts = collections.Counter(seq[i:i+2] for i in range(len(seq)-1))
+    for _ in range(40):
+        newpaircounts = collections.Counter()
+        for pair, c in paircounts.items():
+            p1, p2 = pair
+            newpaircounts[p1+ins[pair]] += c
+            newpaircounts[ins[pair]+p2] += c
+            charcounts[ins[pair]] += c
+        paircounts = newpaircounts
+
+    charcounts = charcounts.most_common()
+    return charcounts[0][1] - charcounts[-1][1]
+
+
+def test():
+    test_input = """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C""".split('\n')
+    assert part1(test_input) == 1588
+    assert part2(test_input) == 2188189693529
+
+
+if __name__ == "__main__":
+    test()
+    vals = input_as_lines('C:\\Repos\\Privat\\AdventOfCode\\2021\\input_day14.txt')
+    print(f"Part 1: {part1(vals)}")
+    print(f"Part 2: {part2(vals)}")
