62358437dee5b9e6e9d8dcd4a4bd10269f7fb4fd
Rasmus Aaen <rasmus@aaen.net>
2021-12-16 17:24:49 +0100

    2021 day 16

--- 2021/aoc.py
+++ 2021/aoc.py
@@ -1,6 +1,8 @@
 ﻿# aoc.py
 from typing import List
 
+
+## Input functions
 def input_as_string(filename:str) -> str:
     """returns the content of the input file as a string"""
     with open(filename, encoding='utf-8') as f:
@@ -15,3 +17,38 @@ def input_as_ints(filename:str) -> List[int]:
     lines = input_as_lines(filename)
     line_as_int = lambda l: int(l.rstrip('\n'))
     return list(map(line_as_int, lines))
+
+def input_as_intGrid(filename:str) -> List[List[int]]:
+    """Return a grid of integers"""
+    lines = input_as_lines(filename)
+    return list(list(map(int,row)) for row in lines)
+
+
+## Helper functions
+def getNeighbours4(r, c, h, w):
+    #Get valid neighbours for a grid point (up, down, left, right)
+    for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+        rr, cc = (r + dr, c + dc)
+        if 0 <= rr < w and 0 <= cc < h:
+            yield rr, cc
+
+def getNeighbours8(r, c, h, w):
+    #Get valid neighbours for a grid point (up, down, left, right)
+    for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1)):
+        rr, cc = (r + dr, c + dc)
+        if 0 <= rr < w and 0 <= cc < h:
+            yield rr, cc
+
+
+## Shortcuts
+
+# A double for loop can be replaced by itertools.product():
+
+#     for r in range(rows):
+#         for c in range(cols):
+#             grid[r][c] += 1
+
+# can be written as:
+
+#     for r,c in product(range(rows), range(cols)):
+#         grid[r][c] += 1
---
+++ 2021/day16.py
@@ -0,0 +1,91 @@
+﻿import collections
+import math
+from aoc import *
+
+def parPacket(p: str, pos: int, versions: list):
+
+    ver = int(p[pos:pos+3],2)
+    pos += 3
+    typ = int(p[pos:pos+3],2)
+    pos += 3
+    versions.append(ver)
+    if typ == 4: #literal
+        val = ''
+        while True:
+            grp = p[pos:pos+5]
+            val += grp[1:]
+            pos += 5
+            if grp[0] == '0':
+                break
+        return int(val, 2), pos
+
+    else:
+        vals = []
+        lTyp = p[pos:pos+1]
+        pos += 1
+        if lTyp == '0':
+            pLen = int(p[pos:pos+15], 2)
+            pos += 15
+            orgPos = pos
+            while pos < orgPos + pLen:
+                pac, pos = parPacket(p, pos, versions)
+                vals.append(pac)
+            return calc(vals, typ), pos
+
+        else:
+            pNum = int(p[pos:pos+11], 2)
+            pos += 11
+            for _ in range(pNum):
+                pac, pos = parPacket(p, pos, versions)
+                vals.append(pac)
+            return calc(vals, typ), pos
+
+def calc(vals: list, op: int):
+    if op == 0:
+        return sum(vals)
+    elif op == 1:
+        return math.prod(vals)
+    elif op == 2:
+        return min(vals)
+    elif op == 3:
+        return max(vals)
+    elif op == 5:
+        return 1 if vals[0] > vals[1] else 0
+    elif op == 6:
+        return 1 if vals[0] < vals[1] else 0
+    elif op == 7:
+        return 1 if vals[0] == vals[1] else 0
+
+
+def part1(values: str) -> int:
+    b  =  ''
+    for c in values:
+        b += f'{int(c,16):04b}'
+    ver = []
+    _ = parPacket(b, 0, ver)
+
+    return sum(ver)
+
+
+def part2(values: list) -> int:
+    b  =  ''
+    for c in values:
+        b += f'{int(c,16):04b}'
+    ver = []
+    res, _ = parPacket(b, 0, ver)
+
+    return res
+
+
+def test():
+    test_input = """8A004A801A8002F478"""
+    assert part1(test_input) == 16
+    test_input = """9C0141080250320F1802104A08"""
+    assert part2(test_input) == 1
+
+
+if __name__ == "__main__":
+    test()
+    vals = input_as_string('C:\\Repos\\Privat\\AdventOfCode\\2021\\input_day16.txt')
+    print(f"Part 1: {part1(vals)}")
+    print(f"Part 2: {part2(vals)}")
