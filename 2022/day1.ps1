a6efa7861bc5cecbabd795f1f49d758dff02e9de
Rasmus Aaen <ra@emd.dk>
2022-12-05 13:15:01 +0100

    Day 1 & 2

---
+++ 2022/day1.ps1
@@ -0,0 +1,15 @@
+﻿$in = Get-Content .\input_day1.txt
+$cals = @()
+$c = 0
+foreach($ln in $in) {
+  if($ln -eq '') {
+    $cals += $c
+    $c = 0
+  } else {
+    $c += [int]$ln
+  }
+}
+$cals | sort -Descending| select -first 1
+
+($cals | sort -Descending | select -First 3 | measure -Sum).Sum
+
---
+++ 2022/day2.ps1
@@ -0,0 +1,86 @@
+﻿$in = Get-Content .\input_day2.txt
+
+$score = 0
+
+$sc = @{
+  'X' = 1
+  'Y' = 2
+  'Z' = 3
+}
+
+foreach($round in $in) {
+  $op,$me = $round -split ' '
+  $score += $sc[$me]
+  if($me -eq 'X') {
+    if($op -eq 'A') {
+      $score += 3
+    } elseif ($op -eq 'C') {
+      $score += 6
+    }
+  }
+  if($me -eq 'Y') {
+    if($op -eq 'B') {
+      $score += 3
+    } elseif ($op -eq 'A') {
+      $score += 6
+    }
+  }
+  if($me -eq 'Z') {
+    if($op -eq 'C') {
+      $score += 3
+    } elseif ($op -eq 'B') {
+      $score += 6
+    }
+  }
+}
+$score
+
+$score = 0
+
+foreach ($round in $in) {
+  $op,$res = $round -split ' '
+  switch ($res) {
+    'X' {}
+    'Y' { $score += 3}
+    'Z' { $score += 6}
+  }
+
+  if($op -eq 'A') {
+    if($res -eq 'X') {
+      $me = 'Z'
+      $score += $sc[$me]
+    } elseif ($res -eq 'Y') {
+      $me = 'X'
+      $score += $sc[$me]
+    } else {
+      $me = 'Y'
+      $score += $sc[$me]
+    }
+  }
+  if($op -eq 'B') {
+    if($res -eq 'X') {
+      $me = 'X'
+      $score += $sc[$me]
+    } elseif ($res -eq 'Y') {
+      $me = 'Y'
+      $score += $sc[$me]
+    } else {
+      $me = 'Z'
+      $score += $sc[$me]
+    }
+  }
+  if($op -eq 'C') {
+    if($res -eq 'X') {
+      $me = 'Y'
+      $score += $sc[$me]
+    } elseif ($res -eq 'Y') {
+      $me = 'Z'
+      $score += $sc[$me]
+    } else {
+      $me = 'X'
+      $score += $sc[$me]
+    }
+  }
+}
+
+$score
