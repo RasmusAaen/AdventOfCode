07d65ce3d6f0662ce4ae54713fbef01254298ab0
Rasmus Aaen <ra@emd.dk>
2022-12-06 10:17:57 +0100

    day 4+5

---
+++ 2022/day4.ps1
@@ -0,0 +1,28 @@
+﻿$in = Get-Content .\input_day4.txt
+
+$cnt = 0
+foreach ($ln in $in) {
+  $e1,$e2 = $ln -split ','
+  [int]$e1a,[int]$e1b = $e1 -split '-'
+  [int]$e2a,[int]$e2b = $e2 -split '-'
+  if(($e1a -le $e2a -and $e1b -ge $e2b) -or ($e2a -le $e1a -and $e2b -ge $e1b)) {
+    $cnt ++
+  }
+}
+$cnt
+
+$cnt = 0
+foreach ($ln in $in) {
+  $e1,$e2 = $ln -split ','
+  [int]$e1a,[int]$e1b = $e1 -split '-'
+  [int]$e2a,[int]$e2b = $e2 -split '-'
+  $r1 = $e1a..$e1b
+  $r2 = $e2a..$e2b
+  foreach($num in $r1) {
+    if ($r2 -contains $num) {
+      $cnt++
+      break
+    }
+  }
+}
+$cnt
---
+++ 2022/day5.ps1
@@ -0,0 +1,70 @@
+﻿$in = Get-Content -Raw .\input_day5.txt
+$stacks, $moves = $in.Split([System.Environment]::NewLine + [System.Environment]::NewLine,[System.StringSplitOptions]::RemoveEmptyEntries)
+$stacks = $stacks.Split([System.Environment]::NewLine)
+$stacks[-1] -match '(\d+)$' | Out-Null
+[int]$numStacks = $Matches[0]
+
+$moves = $moves.Split([System.Environment]::NewLine,[System.StringSplitOptions]::RemoveEmptyEntries)
+
+$stacklist = [System.Collections.ArrayList]::new()
+for ($i=0; $i -le $numStacks; $i++) {
+  $stackList.Add([System.Collections.Stack]::new()) | Out-Null
+}
+
+for ($i=$stacks.Length-2; $i -ge 0; $i--) {
+  $cnt = 1
+  for($j = 1; $j -lt $stacks[$i].length; $j += 4){
+    $v = $stacks[$i][$j]
+    if ($v -ne ' ') {
+      $stacklist[$cnt].Push($v)
+    }
+    $cnt++
+  }
+}
+
+foreach($move in $moves) {
+  $move -match 'move (\d+) from (\d+) to (\d+)' | Out-Null
+  $num,$from,$to = $matches[1..3]
+  for($i = 0; $i -lt $num; $i++) {
+    $stacklist[$to].Push($stacklist[$from].Pop())
+  }
+}
+
+$res = ''
+for($i = 1;$i -lt $stacklist.Count; $i++){
+  $res += $stacklist[$i].Peek()
+}
+write-host ("Part 1: " + $res)
+
+$stacklist = [System.Collections.ArrayList]::new()
+for ($i=0; $i -le $numStacks; $i++) {
+  $stackList.Add([System.Collections.Stack]::new()) | Out-Null
+}
+
+for ($i=$stacks.Length-2; $i -ge 0; $i--) {
+  $cnt = 1
+  for($j = 1; $j -lt $stacks[$i].length; $j += 4){
+    $v = $stacks[$i][$j]
+    if ($v -ne ' ') {
+      $stacklist[$cnt].Push($v)
+    }
+    $cnt++
+  }
+}
+
+foreach($move in $moves) {
+  $move -match 'move (\d+) from (\d+) to (\d+)' | Out-Null
+  $num,$from,$to = $matches[1..3]
+  $tmp = [System.Collections.Stack]::new()
+  for($i = 0; $i -lt $num; $i++) {
+    $tmp.Push($stacklist[$from].Pop())
+  }
+  for($i = 0; $i -lt $num; $i++) {
+    $stacklist[$to].Push($tmp.Pop())
+  }
+}
+$res = ''
+for($i = 1;$i -lt $stacklist.Count; $i++){
+  $res += $stacklist[$i].Peek()
+}
+write-host ("Part 2: " + $res)
