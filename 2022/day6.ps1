59a3263a73e8d9460b98047b1be8af9e4fe069b9
Rasmus Aaen <ra@emd.dk>
2022-12-06 10:36:48 +0100

    Day 6

---
+++ 2022/day6.ps1
@@ -0,0 +1,17 @@
+﻿$in = Get-Content -Raw .\input_day6.txt
+
+for($i = 3; $i -lt $in.Length; $i++){
+  $uniq = ($in[($i-3)..$i] | select -Unique).count
+  if($uniq -eq 4) {
+    $i+1
+    break
+  }
+}
+
+for($i = 14; $i -lt $in.Length; $i++){
+  $uniq = ($in[($i-14)..$i] | select -Unique).count
+  if($uniq -eq 14) {
+    $i+1
+    break
+  }
+}
